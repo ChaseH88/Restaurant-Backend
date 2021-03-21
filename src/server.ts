980bb4fb795296ef82004b4f2e423889899dd6d3
Chase Harrison <chaseh88@gmail.com>
2021-03-20 22:48:47 -0500

    Adding initial types to schema

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import { GraphQLServer } from 'graphql-yoga';
 import resolvers from './resolvers';
 
 export default new GraphQLServer({
-  typeDefs: "src/schema.graphql",
+  typeDefs: [
+    "src/schema.graphql"
+  ],
   resolvers,
   context: req => ({ ...req })
 });
