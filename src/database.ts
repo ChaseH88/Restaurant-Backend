591904d1dd238c7b76078f77f710019767475cb3
Chase Harrison <chaseh88@gmail.com>
2021-03-21 07:24:53 -0500

    Added mongoose/mongodb connection class

---
+++ src/database.ts
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+interface DatabaseInterface {
+  start(): void
+}
+
+class Database implements DatabaseInterface {
+
+  private connection: string = '';
+  private username: string   = 'chase123';
+  private password: string   = 'chase123';
+  private collection: string = 'restaurant-backend';
+
+  constructor(username?: string, password?: string, collection?: string){
+
+    // Update variables if provided
+    if(Array.prototype.slice.call(arguments).length){
+      this.username = username!;
+      this.password = password!;
+      this.collection = collection!;
+    }
+
+    // Set the connection string
+    this.connectionString();
+
+  }
+
+  // Format the connection string with the local variables
+  private connectionString = (): void => {
+    this.connection = `mongodb+srv://${this.username}:${this.password}@chaseharrison.h2q0q.mongodb.net/${this.collection}?retryWrites=true&w=majority`
+  };
+
+  // Initializes the connection with the database
+  public start = async (): Promise<void> => {
+    try {
+      await mongoose.connect(this.connection, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      return console.log('Database connection successful!');
+    }
+    catch(err){
+      return console.error(`ERROR: ${err}`)
+    }
+  }
+
+}
+
+export { Database };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import server from './server';
+import { Database } from './database';
+
+const db = new Database();
+db.start();
 
 server.start(() =>
   console.log('Server is running on http://localhost:4000/')
