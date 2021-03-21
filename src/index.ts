c646fadc418dc9fe05fbaa28379ca54a0a3ddc29
Chase Harrison <chaseh88@gmail.com>
2021-03-20 21:36:09 -0500

    Added graphql-yoga setup file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Work in progress...');
+import { GraphQLServer } from 'graphql-yoga'
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
+  },
+}
+
+new GraphQLServer({ typeDefs, resolvers }).start(() =>
+  console.log('Server is running on localhost:4000')
+)
